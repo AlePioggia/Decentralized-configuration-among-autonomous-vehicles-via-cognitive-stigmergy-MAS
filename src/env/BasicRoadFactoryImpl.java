eff33c76fcc37b97d687326bc0f3501940ea6b00
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-08-04 16:42:11 +0200

    feat: implement factory in order to generate horizontal and vertical roads

--- src/env/BasicRoadFactoryImpl.java
+++ src/env/BasicRoadFactoryImpl.java
@@ -4,13 +4,28 @@ import java.util.stream.IntStream;
 
 public class BasicRoadFactoryImpl implements RoadFactory {
     @Override
-    public Road create(int x, int length, int y1, int y2) {
+    public Road createHorizontalRoad(int x, int length, int y1, int y2) {
         List<Cell> mainLane = new ArrayList<>();
         List<Cell> secondLane = new ArrayList<>();
 
         IntStream.range(x, x + length).forEach(i -> {
-            Cell mainCell = new Cell(i, y1, "East");
-            Cell secondCell = new Cell(i, y2, "West");
+            Cell mainCell = new Cell(i, y1, "West");
+            Cell secondCell = new Cell(i, y2, "East");
+            mainLane.add(mainCell);
+            secondLane.add(secondCell);
+        });
+
+        return new Road("roadName", mainLane, secondLane);
+    }
+
+    @Override
+    public Road createVerticalRoad(int x1, int x2, int y, int length) {
+        List<Cell> mainLane = new ArrayList<>();
+        List<Cell> secondLane = new ArrayList<>();
+
+        IntStream.range(y, y + length).forEach(i -> {
+            Cell mainCell = new Cell(x1, i, "North");
+            Cell secondCell = new Cell(x2, i, "South");
             mainLane.add(mainCell);
             secondLane.add(secondCell);
         });
--- src/env/RoadFactory.java
+++ src/env/RoadFactory.java
@@ -1,3 +1,5 @@
 public interface RoadFactory {
-    Road create(int x, int length, int y1, int y2);
+    public Road createHorizontalRoad(int x, int length, int y1, int y2);
+
+    public Road createVerticalRoad(int x1, int x2, int y, int length);
 }
