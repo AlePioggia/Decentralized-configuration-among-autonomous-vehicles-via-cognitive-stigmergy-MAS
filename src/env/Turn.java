5a17e537dd023ea0f1e6f1b21cfac5a427b022d0
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-08-04 16:41:08 +0200

    feat: update computeTurnType and add equals and hashcode

--- src/env/Turn.java
+++ src/env/Turn.java
@@ -11,11 +11,13 @@ public class Turn {
 
     public String computeTurnType() {
         if (this.fromPosition.getX() < this.toPosition.getX()) {
-            this.turnType = "Right";
+            this.turnType = "east";
         } else if (this.fromPosition.getX() > this.toPosition.getX()) {
-            this.turnType = "Left";
-        } else {
-            this.turnType = "Straight";
+            this.turnType = "west";
+        } else if (this.fromPosition.getY() < this.toPosition.getY()) {
+            this.turnType = "north";
+        } else if (this.fromPosition.getY() > this.toPosition.getY()) {
+            this.turnType = "south";
         }
         return this.turnType;
     }
@@ -31,4 +33,42 @@ public class Turn {
     public String getTurnType() {
         return this.turnType;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((fromPosition == null) ? 0 : fromPosition.hashCode());
+        result = prime * result + ((toPosition == null) ? 0 : toPosition.hashCode());
+        result = prime * result + ((turnType == null) ? 0 : turnType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Turn other = (Turn) obj;
+        if (fromPosition == null) {
+            if (other.fromPosition != null)
+                return false;
+        } else if (!fromPosition.equals(other.fromPosition))
+            return false;
+        if (toPosition == null) {
+            if (other.toPosition != null)
+                return false;
+        } else if (!toPosition.equals(other.toPosition))
+            return false;
+        if (turnType == null) {
+            if (other.turnType != null)
+                return false;
+        } else if (!turnType.equals(other.turnType))
+            return false;
+        return true;
+    }
+
 }
