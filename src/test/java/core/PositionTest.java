1927877cff8b9c1cbb2bc2e7ba32b33e2ba6398c
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-08-07 18:00:24 +0200

    chore: update configuration file to make java tests work

---
+++ src/test/java/core/PositionTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+    @Test
+    public void testJunit() {
+        assertEquals(1, 1);
+    }
+}
