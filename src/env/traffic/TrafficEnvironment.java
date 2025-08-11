006da21b30fdbfe44d1e2375fd3865e1c701f38f
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-08-11 09:58:53 +0200

    test: implement Utils tests

--- src/env/traffic/TrafficEnvironment.java
+++ src/env/traffic/TrafficEnvironment.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Timer;
-import java.util.TimerTask;
 
 import cartago.Artifact;
 import cartago.INTERNAL_OPERATION;
---
+++ src/test/java/core/UtilsTest.java
@@ -0,0 +1,33 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Timer;
+
+public class UtilsTest {
+    @Test
+    public void testComputeNextPosition() {
+        Cell current = new Cell(0, 0, "East");
+        Position expectedPosition = new Position(1, 0);
+        Position actualPosition = Utils.computeNextPosition(current);
+        assertEquals(expectedPosition, actualPosition);
+    }
+
+    @Test
+    public void testParseTurnAction() {
+        String action = "turn: 0, 1";
+        Position expectedPosition = new Position(0, 1);
+        Position actualPosition = Utils.parseTurnAction(action);
+        assertEquals(expectedPosition, actualPosition);
+    }
+
+    @Test
+    public void testCreateTimer() {
+        Timer timer = Utils.createTimer("TestTimer", () -> System.out.println("Timer triggered"), 2, 2);
+        assertNotNull(timer);
+    }
+
+}
