559a6dffbafbd5dbb1d9210d21b954e70a96d96e
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-08-21 17:05:08 +0200

    feat: create standard class for defining simulation environment layout

---
+++ src/env/road/RoadLayout.java
@@ -0,0 +1,38 @@
+package road;
+
+import java.util.List;
+import java.util.Set;
+
+import core.Position;
+
+public class RoadLayout {
+    private final List<Road> roads;
+    private final Set<Set<Position>> intersectionFootprints;
+    private final List<Position> trafficLightPositions;
+    private final Set<Position[]> turns;
+
+    public RoadLayout(List<Road> roads, Set<Set<Position>> intersectionFootprints, List<Position> trafficLightPositions,
+            Set<Position[]> roadSegments) {
+        this.roads = roads;
+        this.intersectionFootprints = intersectionFootprints;
+        this.trafficLightPositions = trafficLightPositions;
+        this.turns = roadSegments;
+    }
+
+    public List<Road> getRoads() {
+        return this.roads;
+    }
+
+    public Set<Set<Position>> getIntersectionFootprints() {
+        return this.intersectionFootprints;
+    }
+
+    public List<Position> getTrafficLightPositions() {
+        return this.trafficLightPositions;
+    }
+
+    public Set<Position[]> getTurns() {
+        return this.turns;
+    }
+
+}
